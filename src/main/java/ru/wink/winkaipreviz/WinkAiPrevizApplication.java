27aecc57bd4f450c5adb5b117131dd56e8ff45a9
wbryon <habibula.tamirbudaev@gmail.com>
2025-11-01 18:02:31 +0300

    feat: init commit

---
+++ src/main/java/ru/wink/winkaipreviz/WinkAiPrevizApplication.java
@@ -0,0 +1,13 @@
+package ru.wink.winkaipreviz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinkAiPrevizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinkAiPrevizApplication.class, args);
+	}
+
+}
